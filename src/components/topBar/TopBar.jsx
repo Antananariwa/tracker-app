1a374baf899a5c9a4d726927c6781ed019eede4f
Your Name <208674117+Antananariwa@users.noreply.github.com>
2026-03-19 16:35:40 +0000

    TopBar internal structure and positioning of content

--- src/components/topBar/TopBar.jsx
+++ src/components/topBar/TopBar.jsx
@@ -4,7 +4,9 @@ import './TopBar.css';
 const TopBar = () => {
   return (
     <div className = "topBarDiv">
-      
+      <div className='leftGroup'></div>
+      <div className='centerGroup'></div>
+      <div className='rightGroup'></div>
     </div>
   )
 }

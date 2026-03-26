0fca87cd4ce2bbd795d0560591a308380c8eb7f5
Antananariwa <208674117+Antananariwa@users.noreply.github.com>
2026-03-26 20:18:15 +0000

    create basic LoginPage component

---
+++ src/components/mainContent/LoginPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './LoginPage.css'
+import MainContentBox from './MainContentBox';
+
+const LoginPage = () => {
+  return (
+    <div>
+      <MainContentBox>
+        <h2>username/email</h2>
+      </MainContentBox>
+    </div>
+  )
+}

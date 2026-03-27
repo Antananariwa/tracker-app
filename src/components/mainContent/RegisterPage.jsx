a754b6ee7c93adfe220b6d2bc7936b09ded80cda
Your Name <208674117+Antananariwa@users.noreply.github.com>
2026-03-27 20:00:51 +0000

    First draft of register page

---
+++ src/components/mainContent/RegisterPage.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './RegisterPage.css'
+import MainContentBox from './MainContentBox'
+
+const RegisterPage = () => {
+  return (
+    <div className='registerPageWrapper'>
+      <MainContentBox>
+        <h4>Username</h4>
+        <input></input>
+        <br/>
+        <br/>
+        <h4>Email</h4>
+        <input></input>
+        <h4>Confirm Email</h4>
+        <input></input>
+        <br/>
+        <br/>
+        <h4>Password</h4>
+        <input></input>
+        <br/>
+        <br/>
+        <h4>Confirm Password</h4>
+        <input></input>
+      </MainContentBox>
+    </div>
+  )
+}
+
+export default RegisterPage

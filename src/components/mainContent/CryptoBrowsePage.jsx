a77cd7da2baa780ab45c6f8a4a2e4e9733dadd5d
Your Name <208674117+Antananariwa@users.noreply.github.com>
2026-04-01 23:35:33 +0100

    add hook data, basics

--- src/components/mainContent/CryptoBrowsePage.jsx
+++ src/components/mainContent/CryptoBrowsePage.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import './CryptoBrowsePage.css'
 import MainContentBox from './MainContentBox'
+import useCryptoApiHook from '../../../scripts/fetchCryptoData.js'
 
 const CryptoBrowsePage = () => {
+  const { data, loading, error } = useCryptoApiHook();
+
+  console.log('Crypto data:', data);
+  console.log('Loading:', loading);
+  console.log('Error:', error);
+
   return (
     <div>
       <MainContentBox>

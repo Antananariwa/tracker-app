e1e1c9c2d36c1ac1dccb1f4459c269cfe5d44c90
Your Name <208674117+Antananariwa@users.noreply.github.com>
2026-04-01 23:43:19 +0100

    add new component to main container

--- src/components/mainContent/MainContentContainer.jsx
+++ src/components/mainContent/MainContentContainer.jsx
@@ -6,6 +6,7 @@ import PrimaryGraph from './displays/groups/PrimaryGraph.jsx';
 import StockMainPage from './StockMainPage.jsx';
 import LoginPage from './LoginPage.jsx'
 import RegisterPage from './RegisterPage.jsx';
+import CryptoBrowsePage from './CryptoBrowsePage.jsx';
 
 
 const MainContentContainer = () => {
@@ -14,7 +15,7 @@ const { selectedMainPage } = useMainPage();
   return (
     <div className="MainContentConainerDiv">
         {selectedMainPage === 'stocks' && <StockMainPage/>}
-        {selectedMainPage === 'crypto' && <CryptoMainPage />}
+        {selectedMainPage === 'crypto' && <CryptoBrowsePage />}
         {selectedMainPage === 'commodities' && <CommoditiesMainPage />}
         {selectedMainPage === 'realEstate' && <RealEstateMainPage />}
 

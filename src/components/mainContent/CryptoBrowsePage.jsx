62a3fa1f7dd5a38515352a0ccba5eee5db2f6b13
Your Name <208674117+Antananariwa@users.noreply.github.com>
2026-04-01 23:14:53 +0100

    basic layout

--- src/components/mainContent/CryptoBrowsePage.jsx
+++ src/components/mainContent/CryptoBrowsePage.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './CryptoBrowsePage.css'
+import MainContentBox from './MainContentBox'
 
 const CryptoBrowsePage = () => {
   return (
     <div>
+      <MainContentBox>
+        {/*search bar*/}
+      </MainContentBox>
+
+      <MainContentBox>
+        {/*graph*/}
+      </MainContentBox>
       
     </div>
   )

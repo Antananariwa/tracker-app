01282e75e2f3fd9fa70b7cc9642fee7bb853b895
Your Name <208674117+Antananariwa@users.noreply.github.com>
2026-04-02 15:58:42 +0100

    lates coin price

--- src/components/mainContent/CryptoBrowsePage.jsx
+++ src/components/mainContent/CryptoBrowsePage.jsx
@@ -4,15 +4,17 @@ import MainContentBox from './MainContentBox'
 import useCryptoApiHook from '../../../scripts/fetchCryptoData.js'
 
 const CryptoBrowsePage = () => {
-  const { data, loading, error } = useCryptoApiHook();
+  const { data, loading, error } = useCryptoApiHook('bitcoin');
 
   console.log('Crypto data:', data);
-  console.log('Loading:', loading);
-  console.log('Error:', error);
+  // console.log('Loading:', loading);
+  // console.log('Error:', error);
+  const latestCoinPrice = data?.bitcoin?.usd ?? 0;
 
   return (
     <div>
       <MainContentBox>
+        <h2>Bitcoin Price: ${latestCoinPrice}</h2>
         {/*search bar*/}
       </MainContentBox>
 

d0c2fd545a5c92f300a226204207dd1116183532
Your Name <208674117+Antananariwa@users.noreply.github.com>
2026-04-10 20:18:23 +0100

    create portfolio component

---
+++ src/components/mainContent/PortfolioStocksPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PortfolioStocksPage.css';
+
+const PortfolioStocksPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PortfolioStocksPage

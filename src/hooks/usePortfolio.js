42ee876fb79d667eba76e09592e9ab1a4d42c37f
Your Name <208674117+Antananariwa@users.noreply.github.com>
2026-04-11 16:36:16 +0100

    add fetching hook code

--- src/hooks/usePortfolio.js
+++ src/hooks/usePortfolio.js
@@ -2,7 +2,32 @@ import { useState, useEffect } from 'react'
 import { supabase } from '../lib/supabase'
 
 const usePortfolio = () => {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
 
+  useEffect(() => {
+    setLoading(true)
+
+
+    supabase
+      .from('assets')
+      .select('*')
+      .then(({ data, error }) => {
+        if (error) {
+          setError(error)
+          console.error('Supabase eror:', error.message)
+        } else {
+          setData(data)
+        }
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+
+  }, [])
+
+  return { data, loading, error }
 }
 
 export default usePortfolio

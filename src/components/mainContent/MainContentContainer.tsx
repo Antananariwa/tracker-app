1328559a6377919318a1b03d0ad02d31a1808c48
Your Name <208674117+Antananariwa@users.noreply.github.com>
2026-05-22 23:56:29 +0100

    add Route import

--- src/components/mainContent/MainContentContainer.tsx
+++ src/components/mainContent/MainContentContainer.tsx
@@ -4,6 +4,7 @@ import StockMainPage from './StockMainPage';
 import LoginPage from './LoginPage'
 import RegisterPage from './RegisterPage';
 import PortfolioStocksPage from './PortfolioStocksPage'
+import { Routes, Route } from 'react-router-dom'
 
 
 const MainContentContainer = () => {

7c470da75ec5d923b581de98926a278af8a68b6a
Your Name <208674117+Antananariwa@users.noreply.github.com>
2026-05-22 23:08:12 +0100

    import context wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,27 @@ import LeftMenuContainer from './components/leftMenu/LeftMenuContainer'
 import MainContentContainer from './components/mainContent/MainContentContainer'
 import { MainPageProvider } from './context/MainPageContext';
 import TopBar from './components/topBar/TopBar';
+import { BrowserRouter } from 'react-router-dom';
 
 
 function App() {
 
   return (
+
     <MainPageProvider>
-      <div className="bodyWrapper">
-          <TopBar/>
+      <BrowserRouter>
+        <div className="bodyWrapper">
+            <TopBar/>
 
-          <LeftMenuContainer title="Left Container"/>
+            <LeftMenuContainer title="Left Container"/>
 
-        <div className="contentDiv">
-          <Header title="Investments Calculator" subtitle="Table with individual assets"/>
-          <MainContentContainer/>
+          <div className="contentDiv">
+            <Header title="Investments Calculator" subtitle="Table with individual assets"/>
+            <MainContentContainer/>
+          </div>
+
         </div>
-
-      </div>
+      </BrowserRouter>
     </MainPageProvider>
   )
 }

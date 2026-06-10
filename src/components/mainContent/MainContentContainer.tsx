ea41bb036f7f141474c5e4cdbe0a0c9108e6fa6d
Your Name <208674117+Antananariwa@users.noreply.github.com>
2026-06-10 15:37:58 +0100

    adjust import path

--- src/components/mainContent/MainContentContainer.tsx
+++ src/components/mainContent/MainContentContainer.tsx
@@ -4,7 +4,7 @@ import LoginPage from './LoginPage'
 import RegisterPage from './RegisterPage';
 import PortfolioStocksPage from './PortfolioStocksPage'
 import { Routes, Route } from 'react-router-dom'
-import CryptoBrowsePage from './displays/browse/CryptoBrowsePage';
+import CryptoBrowsePage from './browse/CryptoBrowsePage';
 
 
 const MainContentContainer = () => {

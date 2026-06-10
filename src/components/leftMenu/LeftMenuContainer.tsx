b83a30db2d59cab0aadcc46c01e111b830739d88
Your Name <208674117+Antananariwa@users.noreply.github.com>
2026-06-10 15:50:12 +0100

    add buttons manually to containers

--- src/components/leftMenu/LeftMenuContainer.tsx
+++ src/components/leftMenu/LeftMenuContainer.tsx
@@ -1,6 +1,7 @@
 import './LeftMenuContainer.css'
 import LeftMenuBox from './LeftMenuBox'
 import { useNavigate } from 'react-router-dom';
+import Button from '../ui/Button';
 
 type LeftMenuContainerProps = {
   title?: string
@@ -15,17 +16,62 @@ const LeftMenuContainer = ({
     <div className="LeftMenuContainer">
       {title && <h2>{title}</h2>}
 
-      <LeftMenuBox 
-        groupName="Portfolio" 
-        optionName={["Stocks", "Option2", "Option3", "Option4"]} 
-        onOptionClick={()=>navigate('/portfolio/stocks')}
-      />
-      <LeftMenuBox 
-        groupName="Browse" 
-        optionName={["Stocks", "Crypto"]}
-        />
-      <LeftMenuBox groupName="Account" optionName={["Option13", "Option14", "Option15", "Option16"]}/>
-      <LeftMenuBox groupName="Settings" optionName={["Option17", "Option18", "Option19",]}/>
+      <LeftMenuBox groupName="Portfolio" >
+
+            <Button 
+              variant={'secondary'}
+              className={'optionButtons'}
+              onClick={()=>navigate('/portfolio/stocks')}
+            >
+              Stocks
+            </Button>
+
+            <Button 
+              variant={'secondary'}
+              className={'optionButtons'}
+              onClick={()=>navigate('/portfolio/crypto')}
+            >
+              Crypto
+            </Button>
+
+      </LeftMenuBox>
+
+      <LeftMenuBox groupName="Browse">
+            <Button 
+              variant={'secondary'}
+              className={'optionButtons'}
+              onClick={()=>navigate('/browse/stocks')}
+            >
+              Stocks
+            </Button>
+
+            <Button 
+              variant={'secondary'}
+              className={'optionButtons'}
+              onClick={()=>navigate('/browse/crypto')}
+            >
+              Crypto
+            </Button>
+
+      </LeftMenuBox>
+
+      <LeftMenuBox groupName="Account">
+            <Button 
+              variant={'secondary'}
+              className={'optionButtons'}
+              onClick={()=>navigate('/account/option1')}
+            >
+              Option1
+            </Button>
+
+            <Button 
+              variant={'secondary'}
+              className={'optionButtons'}
+              onClick={()=>navigate('/account/option2')}
+            >
+              Option2
+            </Button>
+      </LeftMenuBox>
     </div>
   )
 }

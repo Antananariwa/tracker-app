69cf0675b13c69aa19d0c73508684b59df7c075f
Your Name <208674117+Antananariwa@users.noreply.github.com>
2026-06-11 17:58:09 +0100

    start crafting crypto copy of stockData.ts

--- src/utils/cryptoData.ts
+++ src/utils/cryptoData.ts
@@ -1,5 +1,31 @@
 export type CoinGeckoResponse = {
-  "prices":        [number, number][],
-  "market_caps":   [number, number][],
-  "total_volumes": [number, number][],
+  "prices":        [date: number, price: number][],
+  "market_caps":   [date: number, value: number][],
+  "total_volumes": [date: number, value: number][],
 }
+
+export type LatestCryptoPrice = {
+  date: number
+  price: number
+}
+
+export type GraphTimeFrame = "1M" | "3M" | "6M" | "1Y" | "3Y" | "5Y" | "10Y" | "20Y" 
+
+
+
+export const extractLatestCryptoPrice = (data: CoinGeckoResponse): LatestCryptoPrice | null => {
+  if (!data || !data['prices']) return null;
+  
+  const prices = data['prices'];
+  const lastElement = prices[prices.length - 1];
+
+  if (!lastElement) return null;
+
+  const lastDate = lastElement[0];
+  const latestPrice = lastElement[1];
+
+  return {
+    date: lastDate,
+    price: latestPrice,
+  };
+};

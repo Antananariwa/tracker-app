9d794b75d03da175e3928824bbaaed5d38456754
Your Name <208674117+Antananariwa@users.noreply.github.com>
2026-06-12 18:23:22 +0100

    adjust props and types

--- src/components/mainContent/displays/groups/PrimaryGraph.tsx
+++ src/components/mainContent/displays/groups/PrimaryGraph.tsx
@@ -3,16 +3,19 @@ import ApiDataBox from '../ApiDataBox';
 import DemoGraph from '../graphs/AreaResponsiveContainerGraph';
 import LatestPriceDisplay from '../LatestPriceDisplay';
 import TimeFrameOptions from '../../TimeFrameOptions';
-import type { LatestStockPrice, ChartPriceByDateWeekly, GraphTimeFrame } from '../../../../utils/stockData'
+import type { LatestStockPrice, GraphTimeFrame } from '../../../../utils/stockData'
+import type { ExampleProps } from '../graphs/AreaResponsiveContainerGraph'
 
 type PrimaryGraphProps = {
   latestPriceTitle: string
   loading: boolean
   error: Error | null
   latestPriceData: LatestStockPrice | null
-  chartDataTimeFrame: ChartPriceByDateWeekly
+  chartDataTimeFrame: ExampleProps['chartData']
   setSelectedTimeFrame: (value: GraphTimeFrame) => void
   selectedTimeFrame: GraphTimeFrame
+  XAxisDataKey: ExampleProps['XAxisDataKey']
+  areaDataKey: ExampleProps['areaDataKey']
 }
 
 const PrimaryGraph = ({latestPriceTitle, loading, error, latestPriceData, chartDataTimeFrame, setSelectedTimeFrame, selectedTimeFrame, XAxisDataKey, areaDataKey}: PrimaryGraphProps) => {
@@ -21,7 +24,7 @@ const PrimaryGraph = ({latestPriceTitle, loading, error, latestPriceData, chartD
       <ApiDataBox title = {latestPriceTitle} loading = {loading} error = {error}>
         <LatestPriceDisplay latestPriceData={latestPriceData} />
       </ApiDataBox> 
-      <DemoGraph chartData = {chartDataTimeFrame} XAxisDataKey = {} areaDataKey = {}/>
+      <DemoGraph chartData = {chartDataTimeFrame} XAxisDataKey = {XAxisDataKey} areaDataKey = {areaDataKey}/>
       <TimeFrameOptions
         selectedTimeFrame={selectedTimeFrame}
         onOptionClick={(time) => {

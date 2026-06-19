5c792602107edd5f9efba1b05bca524b73e984a1
Your Name <208674117+Antananariwa@users.noreply.github.com>
2026-06-19 15:50:39 +0100

    adjust crypto page for generic type

--- src/components/mainContent/browse/CryptoBrowsePage.tsx
+++ src/components/mainContent/browse/CryptoBrowsePage.tsx
@@ -1,19 +1,23 @@
 import { useState } from 'react';
 import MainContentBox from "../MainContentBox" 
-import DemoGraph from
+import DemoGraph from '../displays/graphs/AreaResponsiveContainerGraph'
 import CryptoSearchBar from "../searchBars/CryptoSearchBar"
 import useBackendCrypto from '../../../hooks/useBackendCrypto';
-import { extractCoinChartData } from '../../../utils/cryptoData';
+import { extractCoinChartData, adjustDataByTime } from '../../../utils/cryptoData';
+import TimeFrameOptions from '../TimeFrameOptions';
+import type {CryptoGraphTimeFrame} from '../../../utils/cryptoData';
 
 
 const CryptoBrowsePage = () => {
   const [ selectedCrypto, setSelectedCrypto ] = useState('')
-  const [ selectedTimeFrame, setSelectedTimeFrame ] = useState<GraphTimeFrame>('')
+  const [selectedTimeFrame, setSelectedTimeFrame] = useState<CryptoGraphTimeFrame>('1Y')
 
   const {data, loading, error} = useBackendCrypto(selectedCrypto)
 
   const chartData = data ? extractCoinChartData(data) : []
   const chartDataTimeFrame = adjustDataByTime(chartData, selectedTimeFrame)
+
+  const timeRange: CryptoGraphTimeFrame[] = ['1M', '3M', '6M', '1Y']
 
   return (
     <div>
@@ -27,6 +31,12 @@ const CryptoBrowsePage = () => {
         XAxisDataKey = "date" 
         areaDataKey = "price"
         />
+        <TimeFrameOptions
+          selectedTimeFrame={selectedTimeFrame}
+          onOptionClick={(time) => {
+            setSelectedTimeFrame(time);}}
+          timeRange = {timeRange}
+          />
        </MainContentBox>
     </div>
 

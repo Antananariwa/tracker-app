0daa5ecc076c2575a45fe148937f2609c5ad2b73
Your Name <208674117+Antananariwa@users.noreply.github.com>
2026-06-20 18:18:10 +0100

    create crypto asset table

---
+++ src/components/mainContent/portfolio/PortfolioCryptoPage.tsx
@@ -0,0 +1,58 @@
+import MainContentBox from '../MainContentBox';
+import { preparePortfolioAssets } from '../../../utils/stockData'
+import usePortfolio from '../../../hooks/usePortfolio'
+
+const PortfolioCryptoPage = () => {
+  const { data, loading, error } = usePortfolio('crypto');
+  const assets = data ? preparePortfolioAssets(data) : [];
+  let content;
+
+  if (loading) {
+    content = (
+      <MainContentBox>"Please wait while we fetch the data..."</MainContentBox>
+    )
+  } else if (error) {
+    content = (
+      <MainContentBox>{`An error occurred: ${error}`}</MainContentBox>
+    )
+  } else {
+    content = (
+      <MainContentBox>
+        <table>
+          <thead>
+            <tr>
+              <th>Symbol</th>
+              <th>Name</th>
+              <th>Quantity</th>
+              <th>avgBuyPrice</th>
+              <th>purchaseCost</th>
+              <th>status</th>
+              <th>acquiredAt</th>
+            </tr>
+          </thead>
+          <tbody>
+            {assets.map(asset => (
+              <tr key={asset.symbol}>
+                <td>{asset.symbol}</td>
+                <td>{asset.name}</td>
+                <td>{asset.quantity}</td>
+                <td>{asset.avgBuyPrice}</td>
+                <td>{asset.purchaseCost}</td>
+                <td>{asset.status}</td>
+                <td>{asset.acquiredAt}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </MainContentBox>
+    )
+  }
+
+  return (
+    <div>
+      {content}
+    </div>
+  )
+}
+
+export default PortfolioCryptoPage

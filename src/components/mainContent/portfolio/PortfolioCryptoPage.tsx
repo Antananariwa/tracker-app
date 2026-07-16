252071af745b16b85557ad6722725fc9f25e0ac3
Your Name <208674117+Antananariwa@users.noreply.github.com>
2026-07-16 23:01:21 +0100

    add new wrapper div/class

--- src/components/mainContent/portfolio/PortfolioCryptoPage.tsx
+++ src/components/mainContent/portfolio/PortfolioCryptoPage.tsx
@@ -19,32 +19,34 @@ const PortfolioCryptoPage = () => {
   } else {
     content = (
       <MainContentBox>
-        <table className="cryptoHoldingsTable">
-          <thead>
-            <tr>
-              <th>Symbol</th>
-              <th>Name</th>
-              <th className="cryptoNum">Quantity</th>
-              <th className="cryptoNum">avgBuyPrice</th>
-              <th className="cryptoNum">purchaseCost</th>
-              <th>status</th>
-              <th>acquiredAt</th>
-            </tr>
-          </thead>
-          <tbody>
-            {assets.map(asset => (
-              <tr key={asset.symbol}>
-                <td>{asset.symbol}</td>
-                <td>{asset.name}</td>
-                <td className="cryptoNum">{asset.quantity}</td>
-                <td className="cryptoNum">{asset.avgBuyPrice}</td>
-                <td className="cryptoNum">{asset.purchaseCost}</td>
-                <td>{asset.status}</td>
-                <td>{asset.acquiredAt}</td>
+        <div className="cryptoTableScroll">
+          <table className="cryptoHoldingsTable">
+            <thead>
+              <tr>
+                <th>Symbol</th>
+                <th>Name</th>
+                <th className="cryptoNum">Quantity</th>
+                <th className="cryptoNum">avgBuyPrice</th>
+                <th className="cryptoNum">purchaseCost</th>
+                <th>status</th>
+                <th>acquiredAt</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {assets.map(asset => (
+                <tr key={asset.symbol}>
+                  <td>{asset.symbol}</td>
+                  <td>{asset.name}</td>
+                  <td className="cryptoNum">{asset.quantity}</td>
+                  <td className="cryptoNum">{asset.avgBuyPrice}</td>
+                  <td className="cryptoNum">{asset.purchaseCost}</td>
+                  <td>{asset.status}</td>
+                  <td>{asset.acquiredAt}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </MainContentBox>
     )
   }

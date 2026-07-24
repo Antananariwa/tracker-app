19acc3a55310710c915503527714cd79413306f3
Your Name <208674117+Antananariwa@users.noreply.github.com>
2026-07-24 23:13:07 +0100

    add currency and percent formatters for portfolio display

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -3,3 +3,16 @@
 //formatSignedCurrnecy
 //formatPercent
 //formatNumbeer
+
+
+export const formatCurrency = (value: number): string => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(value)
+}
+
+export const formatPercent = (value: number): string => {
+  const sign = value > 0 ? '+' : ''
+  return `${sign}${(value * 100).toFixed(2)}%`
+}

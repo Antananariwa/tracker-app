51124a1dd12d3dae7d4047d578666cae11b5e0e5
Your Name <208674117+Antananariwa@users.noreply.github.com>
2026-07-28 19:32:57 +0100

    create unified portfolio page, showwing all assets, based on stock table with some new stuff, still under construction

---
+++ src/components/mainContent/portfolio/PortfolioAssetsPage.tsx
@@ -0,0 +1,62 @@
+import MainContentBox from "../MainContentBox";
+
+const PortfolioAssetsPage = () => {
+  const { data, loading, error } = usePortfolio('stock');
+  const assets = data ? preparePortfolioAssets(data) : [];
+  let content;
+
+  if (loading) {
+    content = (
+      <MainContentBox>"Please wait while we fetch the data..."</MainContentBox>
+    )
+  } else if (error) {
+    content = (
+      <MainContentBox>{`An error occurred: ${error}`}</MainContentBox>
+    )
+  } else {
+    content = (
+      <MainContentBox>
+        <div className = "assetTableScroll">
+          <table className="assetHoldingsTable">
+            <thead>
+              <tr>
+                <th>Symbol</th>
+                <th>Name</th>
+                <th className="stockNum">Quantity</th>
+                <th className="stockNum">Avg Buy Price</th>
+                <th className="stockNum">Purchase Cost</th>
+                <th>status</th>
+                <th>acquiredAt</th>
+                <th>Current Price</th>
+                <th>Current Value</th>
+                <th>Gain/Loss</th>
+                <th>Gain/Loss percent</th>
+              </tr>
+            </thead>
+            <tbody>
+              {assets.map(asset => (
+                <tr key={asset.symbol}>
+                  <td>{asset.symbol}</td>
+                  <td>{asset.name}</td>
+                  <td className="stockNum">{asset.quantity}</td>
+                  <td className="stockNum">{asset.avgBuyPrice}</td>
+                  <td className="stockNum">{asset.purchaseCost}</td>
+                  <td>{asset.status}</td>
+                  <td>{asset.acquiredAt}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </MainContentBox>
+    )
+  }
+
+  return (
+    <div>
+      {content}
+    </div>
+  )
+}
+
+export default PortfolioAssetsPage

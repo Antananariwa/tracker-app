68aea7768b4c4f1fa4c1cfc70f88ebee582ee700
Your Name <208674117+Antananariwa@users.noreply.github.com>
2026-08-01 19:52:44 +0100

    move new router to old one as specific route

--- backend/routes/stocks.ts
+++ backend/routes/stocks.ts
@@ -1,5 +1,10 @@
 import express, { Request, Response} from 'express'
 import { createClient } from '@supabase/supabase-js'
+import finnhub from 'finnhub'
+
+const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+api_key.apiKey = process.env.FINNHUB_KEY
+const finnhubClient = new finnhub.DefaultApi()
 
 type AlphaVantageWeeklyResponse = {
   'Weekly Time Series'?: {
@@ -10,6 +15,17 @@ type AlphaVantageWeeklyResponse = {
   'Error Message'?: string
   'Information'?: string
   'Note'?: string
+}
+
+type FinnhubDataResponse = {
+  'c': number /** Current price */
+  'd': number /** Change */
+  'dp': number /** Percent change */
+  'h': number /** High price of the day */
+  'l': number /** Low price of the day */
+  'o': number /** Open price of the day */
+  'pc': number /** Previous close price */
+  't': number /** Time */
 }
 
 const router = express.Router()
@@ -136,6 +152,27 @@ router.get('/:symbol', async (req: Request<{ symbol: string }>, res: Response) =
   console.error(`Unhandled error for ${symbol}:`, message)
   res.status(500).json({ error: 'Internal server error.' })
 }
+})
+
+router.get('/:symbol/quote', (req: Request<{ symbol: string }>, res: Response) => {
+  const symbol = req.params.symbol.toUpperCase()
+
+  finnhubClient.quote(symbol, (error: Error | null, data: FinnhubDataResponse) => {
+    if (error) {
+      return res.status(500).json({ error: 'Finnhub Quote API Error' })
+    }
+
+    res.json({
+      current_price: data.c,
+      change: data.d,
+      percent_change: data.dp,
+      high_price_of_the_day: data.h,
+      low_price_of_the_day: data.l,
+      open_price_of_the_day: data.o,
+      previous_close_price: data.pc,
+      time: data.t,
+    })
+  })
 })
 
 export default router
